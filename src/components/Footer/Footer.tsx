c8231184045c58f4e4a83b7d509c8e156b95e2a4
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-10-06 16:44:46 +0530

    updt

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <>
-            <footer className="fixed bottom-0 left-0 w-full bg-slate-700">
+            <footer className="relative bottom-0 left-0 w-full bg-slate-700">
                 <div className="min-h-[100px] flex items-end justify-center">
                     <p className="text-center p-4 text-xs text-white">
                         COPYRIGHTS &copy; 2024. ALL RIGHTS RESERVED BY <b>ONILINESTORE (PVT) LTD</b>
--- src/pages/Auth/Signin.tsx
+++ src/pages/Auth/Signin.tsx
@@ -65,27 +65,29 @@ function Signin() {
     }
     return (
         <>
-            <div className="text-center border-b-2 border-b-slate-200 pb-3 mb-6">
-                <h1 className="text-3xl font-extrabold">Signin</h1>
-            </div>
+            <div className="mt-6 mb-8 py-2">
+                <div className="text-center border-b-2 border-b-slate-200 pb-3 mb-8">
+                    <h1 className="text-3xl font-extrabold mb-5">Signin</h1>
+                </div>
 
-            <div className="container mx-auto mb-10 mt-4">
-                <div className="flex items-center justify-center">
-                    <form onSubmit={submit} className="border border-slate-200 rounded-lg max-w-[500px] min-w-[400px] center p-4 shadow-lg">
-                        <div>
-                            <input type="text" className="text-slate-600 font-sm block mb-2 w-full p-2 border border-slate-300 rounded-lg" onChange={usernameChange} placeholder="Username" />
-                        </div>
-                        <div>
-                            <input className="text-slate-600 font-sm block mb-2 w-full p-2 border border-slate-300 rounded-lg" type="password" onChange={passwordChange} placeholder="Password" />
-                        </div>
-                        <div>
-                            <input className="text-slate-600 font-sm block mb-2 w-full p-2 border border-slate-300 rounded-lg" type="password" onChange={confirmPasswordChange} placeholder="Confirm Password" />
-                        </div>
-                        {error && <div>{error}</div>}
-                        <div>
-                            <button className="border w-full text-white rounded-lg px-4 py-3 font-medium bg-blue-700 hover:bg-blue-600" type="submit">Create Account</button>
-                        </div>
-                    </form>
+                <div className="container mx-auto mb-10 mt-4">
+                    <div className="flex items-center justify-center mb-6">
+                        <form onSubmit={submit} className="border border-slate-200 rounded-lg max-w-[500px] min-w-[400px] center p-4 shadow-lg">
+                            <div>
+                                <input type="text" className="mb-4 text-slate-600 font-sm block w-full p-2 border border-slate-300 rounded-lg" onChange={usernameChange} placeholder="Username" />
+                            </div>
+                            <div>
+                                <input className="mb-4 text-slate-600 font-sm block w-full p-2 border border-slate-300 rounded-lg" type="password" onChange={passwordChange} placeholder="Password" />
+                            </div>
+                            <div>
+                                <input className="mb-4 text-slate-600 font-sm block w-full p-2 border border-slate-300 rounded-lg" type="password" onChange={confirmPasswordChange} placeholder="Confirm Password" />
+                            </div>
+                            {error && <div>{error}</div>}
+                            <div>
+                                <button className="border w-full text-white rounded-lg mt-2 px-4 py-3 font-medium bg-blue-700 hover:bg-blue-600" type="submit">Create Account</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </>
